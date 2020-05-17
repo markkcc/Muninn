e2ef781e5588362bd4fe0e824d41df4220a5b00f
r1fter-1589617948 <mu.gl>
2020-05-16 20:08:20 -0400

    Added buttons to pick scan options

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -9,11 +9,10 @@ class ScansController < ApplicationController
     
     @lookup_target = validate_url(scan["URL"])
 
-    @whoisdata = get_whoisdata(@lookup_target)
-    @virustotaldata = get_virustotaldata(@lookup_target)
-    @screenshot = get_screenshot(@lookup_target)
-    
-    #render plain: @whoisdata
+    @whoisdata = get_whoisdata(@lookup_target) unless params[:scan]["whois_enabled"] == "0"
+    @virustotaldata = get_virustotaldata(@lookup_target) unless params[:scan]["virustotal_enabled"] == "0"
+    @screenshot = get_screenshot(@lookup_target) unless scan["screenshot_enabled"] == "0"
+
   end
 
   def validate_url(url)
@@ -41,22 +40,25 @@ class ScansController < ApplicationController
 
   def get_screenshot(lookup_target)
     global_timeout = 5 #seconds, max time to load
-    begin
-      capabilities = Selenium::WebDriver::Remote::Capabilities.firefox(accept_insecure_certs: true)
-      driver = Selenium::WebDriver.for :firefox, desired_capabilities: capabilities
-      driver.manage.timeouts.implicit_wait = global_timeout
-      driver.manage.timeouts.script_timeout = global_timeout
-      driver.manage.timeouts.page_load = global_timeout
-      driver.navigate.to @lookup_target
-      sleep(3)
-      screenshot_base64 = driver.screenshot_as(:base64)
-      driver.quit
-    rescue Selenium::WebDriver::Error::TimeoutError => toe
-      screenshot_base64 = ""
-      driver.quit
-    rescue Selenium::WebDriver::Error::UnknownError => ue
-      screenshot_base64 = ""
-      driver.quit
+    screenshot_base64 = ""
+    if params[:scan]["screenshot_enabled"] == "1"
+      begin
+        capabilities = Selenium::WebDriver::Remote::Capabilities.firefox(accept_insecure_certs: true)
+        driver = Selenium::WebDriver.for :firefox, desired_capabilities: capabilities
+        driver.manage.timeouts.implicit_wait = global_timeout
+        driver.manage.timeouts.script_timeout = global_timeout
+        driver.manage.timeouts.page_load = global_timeout
+        driver.navigate.to @lookup_target
+        sleep(2.5)
+        screenshot_base64 = driver.screenshot_as(:base64)
+        driver.quit
+      rescue Selenium::WebDriver::Error::TimeoutError => toe
+        screenshot_base64 = ""
+        driver.quit
+      rescue Selenium::WebDriver::Error::UnknownError => ue
+        screenshot_base64 = ""
+        driver.quit
+      end
     end
     return screenshot_base64
   end
