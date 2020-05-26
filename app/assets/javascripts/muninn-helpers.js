3a51e6423cc5107bcc63ab21347e284ff4872bf3
r1fter-1589617948 <mu.gl>
2020-05-25 23:16:58 -0400

    javascript cleanup

---
+++ app/assets/javascripts/muninn-helpers.js
@@ -0,0 +1,96 @@
+//<script>
+  function submitHiddenForm() {
+    var url = document.getElementById("textInput").textContent;
+    document.getElementById("scan_URL").value = url;
+    document.getElementsByName('commit')[0].click();
+    startLoading();
+  }
+
+  async function startLoading() {
+    if (document.getElementById('textInput').textContent == "")
+          return;
+    document.getElementById('animlogo').beginElement();
+    document.getElementById('searchBar').style.opacity = '0';
+    document.getElementById('subButtons').style.opacity = '0';
+    document.getElementById('footer').style.opacity = '0';
+    await new Promise(resolve => setTimeout(resolve, 600));
+    document.getElementById('footer').textContent = "";
+    document.getElementById('searchBar').style.display = "none";
+    document.getElementById('subButtons').style.display = "none";
+    document.getElementById('footer').style.opacity = '1';
+    await new Promise(resolve => setTimeout(resolve, 600));
+    slowLoadFooter("[SCANNING]: ..........");
+    await new Promise(resolve => setTimeout(resolve, 2200));
+    randomLoaderFooter();
+  };
+
+  async function slowLoadFooter(inputstr) {
+        document.getElementById('footer').textContent = "";
+        charsArray = [...inputstr];
+        for (i=0; i < charsArray.length; i++) {
+            document.getElementById('footer').append(charsArray[i]);
+            await new Promise(resolve => setTimeout(resolve, 69));
+        }
+  };
+
+  async function randomLoaderFooter() {
+    var msToWait = Math.floor((Math.random() * 4242) + 6969);
+    for ( i=101; i < msToWait; i+=69) {
+      document.getElementById('footer').textContent = "[SCANNING]: " + i + " / " + msToWait;
+      await new Promise(resolve => setTimeout(resolve, 69));
+    }
+    document.getElementById('footer').textContent = "[SCANNING]: " + msToWait + " / " + msToWait;
+    document.getElementById('animlogo').endElement();
+    await new Promise(resolve => setTimeout(resolve, 1600));
+    slowLoadFooter("[DISENGAGING]");
+    await new Promise(resolve => setTimeout(resolve, 3200));
+    slowLoadFooter("[STANDBY]");
+  }
+
+  function toggleScreenshot() {
+    var screenshotButton = document.getElementsByClassName("baffled")[0];
+
+    if(screenshotButton.textContent == "[ENABLED]")
+      screenshotButton.textContent = "[DISABLED]";
+    else
+      screenshotButton.textContent = "[ENABLED]";
+
+    document.getElementById("scan_screenshot_enabled").click();
+  }
+
+  function toggleVT() {
+    var vtButton = document.getElementsByClassName("baffled")[1];
+
+    if(vtButton.textContent == "[ENABLED]")
+      vtButton.textContent = "[DISABLED]";
+    else
+      vtButton.textContent = "[ENABLED]";
+
+    document.getElementById("scan_virustotal_enabled").click();
+  }
+
+  function toggleWHOIS() {
+    var whoisButton = document.getElementsByClassName("baffled")[2];
+
+    if(whoisButton.textContent == "[ENABLED]")
+      whoisButton.textContent = "[DISABLED]";
+    else
+      whoisButton.textContent = "[ENABLED]";
+
+    document.getElementById("scan_whois_enabled").click();
+  }
+
+  function enableAllonLoad() {
+    document.getElementById("scan_screenshot_enabled").checked = true;
+    document.getElementById("scan_virustotal_enabled").checked = true;
+    document.getElementById("scan_whois_enabled").checked = true;
+
+    console.log("Muninn Status: ARMED | STANDBY");
+
+    var urlinputbox = document.getElementById("textInput");
+    urlinputbox.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+        document.getElementById("search-button").click();
+      }
+    });
+  }
